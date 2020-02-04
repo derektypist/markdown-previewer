9051353261609cce3dd7aa1f3e76907e82553895
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2020-02-04 10:53:00 +0000

    Defined markdown at app.js

--- my-markdown-previewer/src/App.js
+++ my-markdown-previewer/src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+let marked = require('marked');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Set Up Component
+
+class App extends Component {
+  state = {
+    markdown: `# Welcome to my project.
+    The purpose of this project...
+    ## Sub Header
+    [British Computer Society](https://www.bcs.org)
+    Here is some code \`<i class&quot;fa fa-music&quot; aria-hidden=&quot;true&quot;></i>\`.To get the music icon using Font Awesome 4.7.0.
+    Writing an Internal Style Sheet
+    \`\`\` &lt;style&gt; p {font-family: &quot;Roboto&quot;, Arial, Verdana, sans-serif&semi;color: green&semi;}&lt;/style&gt;\`\`\`
+    
+    Favourite Celebrities
+    * Anna Richardson
+    * Kate Garraway
+    * Ruth Langsford
+    * Susanna Reid
+    
+    You can make text **bold** using two asterisks
+    
+    
+    List Items can contain links
+    * [The National League](https://www.thenationalleague.org.uk)
+    * [Cambridge Python User Group](https://www.meetup.com/CamPUG)
+    
+    There are blockquotes > The purpose the BCS is to promote and advance the education and practice of computing for the benefit of the public.
+    
+    Image Links
+    
+    ![Google Logo](http://www.google.com/images/errors/logo_sm.gif)
+    
+    ## Computing
+    
+    * Word Processing - include **Clipart Images, Headders and Footers**
+    * Spreadsheets - include **Charts and Formula**
+    
+    `
+  }
 }
+
 
 export default App;
